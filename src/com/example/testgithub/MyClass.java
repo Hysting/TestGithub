ae321dde6a720f94da16e47d426031955237f110
huangpy <huangshuai77@gmail.com>
2014-10-31 20:43:36 +0800

    ....

--- src/com/example/testgithub/MyClass.java
+++ src/com/example/testgithub/MyClass.java
@@ -1,10 +1,45 @@
 package com.example.testgithub;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
 public class MyClass {
 	int b = 1;
 	int a = 100;
     int c = 100;
 	public MyClass() {
 		// TODO Auto-generated constructor stub
+        class MyThread extends Thread {
+            boolean flag;
+            int count;
+            @Override
+            public void run() {
+                super.run();
+                //根据布尔值判断是因为surface中UI的改变必须在callback的oncreate方法和destroy方法之间
+                while (flag) {
+                    Canvas canvas = null;
+                    canvas.drawColor(Color.WHITE);
+                    Paint paint = new Paint();
+                    paint.setColor(Color.GRAY);
+
+//                Shader shader = new LinearGradient(0,0,)
+//                paint.setShader(shader);
+
+                    canvas.drawRect(40, 40, 160, 160, paint);
+                    paint.setColor(Color.RED);
+                    canvas.drawText(count + "", 100, 100, paint);
+                    count++;
+                    //必须把画布解锁了，才能真正绘制
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+            }
+        }
 	}
 }
